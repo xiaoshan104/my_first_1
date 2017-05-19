1ea7f5766bd894c8a64227384bbf321d98171aa6
zhenchao125 <30752740@qq.com>
2017-05-19 17:01:35 +0800

    自动完成提交

---
+++ Advanced_Day07/data6.php
@@ -0,0 +1,11 @@
+<?php
+header("Content-Type:text/html;charset=utf-8");
+$user = $_POST["user"];
+$pwd = $_POST["pwd"];
+$a = $_POST["a"];
+if($user == "zs"){
+    echo $user . "登录成功" . "你的密码：" . $pwd;
+}else{
+    echo $user . "登录失败". "你的密码：" . $pwd  . $a;
+}
+?>
