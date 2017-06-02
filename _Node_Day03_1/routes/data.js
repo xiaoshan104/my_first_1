2e3714775adff7c695b44cf91a2f06c4e4e42408
zhenchao125 <30752740@qq.com>
2017-06-02 11:41:27 +0800

    自动完成提交

--- _Node_Day03_1/routes/data.js
+++ _Node_Day03_1/routes/data.js
@@ -1,18 +1,26 @@
-/**
- * Created by lzc on 02/06/2017.
- */
 var express = require('express');
 var fs = require("fs");
+var path = require("path"); //处理本地文件的路径
 var router = express.Router();
 
 /* GET home page. */
-router.get('/data1', function(req, res, next) {
+router.get('/data1', function (req, res, next){
+
+    var methodName = req.query.callback;
+    methodName = methodName || "foo";
+
+    var page = req.query.page;
+    page = page || 1;
+    page = page > 3 ? 1 : page;
+
     //先获取到users.json中的数据
-    fs.readFile("../data/users.json", "utf-8", function (err, data){
+    var filePath = path.join(__dirname, "../data/users" + page +".json");
+    fs.readFile(filePath, "utf-8", function (err, data){
+
         //元数据
         res.setHeader("Content-Type", "application/json;charset=utf-8");
-        if(!err){
-            res.send(data);
+        if (!err){
+            res.send(methodName + "(" + data + ")");
         }else{
             res.send(err);
         }
